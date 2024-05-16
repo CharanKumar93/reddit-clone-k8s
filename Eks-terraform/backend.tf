8018a8884e521c17e9c1c1d573b9f00fecf25e5f
Charan Kumar <122191042+CharanKumar93@users.noreply.github.com>
2024-05-16 14:18:28 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "charan-cloud289" # Replace with your actual S3 bucket name
+    bucket = "cloud-299" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-2"
+    region = "ap-south-1"
   }
 }
